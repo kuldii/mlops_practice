b6ca4b119aa68b8f7feb0558a1b3ce28c8ccdba9
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2024-02-28 14:49:51 +0500

    feat: lab1 - create datasets folder if not exist

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,1 +1,6 @@
-print("data_creation.py")
+import os
+
+if not os.path.exists('datasets/train'):
+    os.makedirs('datasets/train')
+if not os.path.exists('datasets/test'):
+    os.makedirs('datasets/test')
