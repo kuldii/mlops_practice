4081b4b6b42a86f1eaa70558447b68a051e0c1e5
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2024-02-29 13:38:42 +0500

    feat: lab1 - split features and target

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,1 +1,10 @@
-print("model_preparation.py")
+import pandas as pd
+
+train_data = pd.read_csv("datasets/train/preprocessed_train_data.csv")
+test_data = pd.read_csv("datasets/test/preprocessed_test_data.csv")
+
+# Split features and target
+y_train = train_data["Survived"]
+X_train = train_data.drop(columns="Survived")
+y_test = test_data["Survived"]
+X_test = test_data.drop(columns="Survived")
