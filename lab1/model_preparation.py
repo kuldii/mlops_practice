2dd1ce3bfb37c0a9c7cc8a9b38d6975c05e96e01
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2024-02-29 14:11:01 +0500

    feat: lab1 - training the model

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
 
 train_data = pd.read_csv("datasets/train/preprocessed_train_data.csv")
 test_data = pd.read_csv("datasets/test/preprocessed_test_data.csv")
@@ -8,3 +10,11 @@ y_train = train_data["Survived"]
 X_train = train_data.drop(columns="Survived")
 y_test = test_data["Survived"]
 X_test = test_data.drop(columns="Survived")
+
+# Training the model
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+# Save the model
+with open('lab1/model.pkl', 'wb') as f:
+    pickle.dump(lr, f)
