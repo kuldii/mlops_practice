55729a53902e424baa1f562d72509ef8f43155fa
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2024-02-29 14:21:11 +0500

    feat: lab1 - fix the training model

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -3,13 +3,10 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 
 train_data = pd.read_csv("datasets/train/preprocessed_train_data.csv")
-test_data = pd.read_csv("datasets/test/preprocessed_test_data.csv")
 
 # Split features and target
 y_train = train_data["Survived"]
 X_train = train_data.drop(columns="Survived")
-y_test = test_data["Survived"]
-X_test = test_data.drop(columns="Survived")
 
 # Training the model
 lr = LogisticRegression()
